fadf584ddd5b12681119f56c7fc79182302478b3
wenming762 <398715451@qq.com>
2024-08-06 11:33:17 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.djy5.cn": dockerHub,
+  "quay.djy5.cn": "https://quay.io",
+  "gcr.djy5.cn": "https://gcr.io",
+  "k8s-gcr.djy5.cn": "https://k8s.gcr.io",
+  "k8s.djy5.cn": "https://registry.k8s.io",
+  "ghcr.djy5.cn": "https://ghcr.io",
+  "cloudsmith.djy5.cn": "https://docker.cloudsmith.io",
+  "ecr.djy5.cn": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
